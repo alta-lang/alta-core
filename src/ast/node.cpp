21037b1d57762810815b3f841d394975fa35c81f
Ariel Abreu <facekapow@outlook.com>
2018-12-17 21:31:57 -0500

    Give AST nodes unique UUIDs

--- src/ast/node.cpp
+++ src/ast/node.cpp
@@ -1,7 +1,15 @@
 #include "../../include/altacore/ast/node.hpp"
+#include <sstream>
+#include <crossguid/guid.hpp>
 
 const AltaCore::AST::NodeType AltaCore::AST::Node::nodeType() {
   return NodeType::Node;
 };
 
 void AltaCore::AST::Node::detail(std::shared_ptr<AltaCore::DET::Scope> scope) {};
+
+AltaCore::AST::Node::Node() {
+  std::stringstream uuidStream;
+  uuidStream << xg::newGuid();
+  id = uuidStream.str();
+};
