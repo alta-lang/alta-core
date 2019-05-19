f1cce1dc7c7823402cfa1760538a42c2d14d591b
Ariel Abreu <facekapow@outlook.com>
2019-05-19 08:18:50 -0400

    [general] Do the same for <variant>

--- include/altacore/variant.hpp
+++ include/altacore/variant.hpp
@@ -1,7 +1,7 @@
 #ifndef ALTACORE_VARIANT_HPP
 #define ALTACORE_VARIANT_HPP
 
-#if defined(__has_include) && __has_include(<variant>)
+#if defined(__has_include) && __has_include(<variant>) && !defined(ALTACORE_NO_SYSTEM_VARIANT)
 #include <variant>
 #define ALTACORE_VARIANT std::variant
 #define ALTACORE_VARIANT_HOLDS_ALTERNATIVE std::holds_alternative
