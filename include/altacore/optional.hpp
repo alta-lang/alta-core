8f2251a0982e9a75753492a6e0c50f48528029ce
Ariel Abreu <facekapow@outlook.com>
2024-02-22 23:31:18 -0500

    Remove any, optional, and variant submodules
    All supported up-to-date platforms should support these now

--- include/altacore/optional.hpp
+++ include/altacore/optional.hpp
@@ -1,16 +1,9 @@
 #ifndef ALTACORE_OPTIONAL_HPP
 #define ALTACORE_OPTIONAL_HPP
 
-#if defined(__has_include) && __has_include(<optional>)
 #include <optional>
 #define ALTACORE_OPTIONAL std::optional
 #define ALTACORE_NULLOPT std::nullopt
 #define ALTACORE_MAKE_OPTIONAL std::make_optional
-#else
-#include <optional.hpp>
-#define ALTACORE_OPTIONAL tl::optional
-#define ALTACORE_NULLOPT tl::nullopt
-#define ALTACORE_MAKE_OPTIONAL tl::make_optional
-#endif
 
 #endif /* ALTACORE_OPTIONAL_HPP */
--- include/altacore/variant.hpp
+++ include/altacore/variant.hpp
@@ -1,18 +1,10 @@
 #ifndef ALTACORE_VARIANT_HPP
 #define ALTACORE_VARIANT_HPP
 
-#if defined(__has_include) && __has_include(<variant>) && !defined(ALTACORE_NO_SYSTEM_VARIANT)
 #include <variant>
 #define ALTACORE_VARIANT std::variant
 #define ALTACORE_VARIANT_HOLDS_ALTERNATIVE std::holds_alternative
 #define ALTACORE_VARIANT_GET std::get
 #define ALTACORE_VARIANT_GET_IF std::get_if
-#else
-#include <mpark/variant.hpp>
-#define ALTACORE_VARIANT mpark::variant
-#define ALTACORE_VARIANT_HOLDS_ALTERNATIVE mpark::holds_alternative
-#define ALTACORE_VARIANT_GET mpark::get
-#define ALTACORE_VARIANT_GET_IF mpark::get_if
-#endif
 
 #endif /* ALTACORE_VARIANT_HPP */
